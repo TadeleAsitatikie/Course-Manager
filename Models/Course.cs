c2358fb5c02af11fdcd2aed4f9268737964cc405
DESKTOP-HN3LVDD\Tadele <tadele.asitatikie@gmail.com>
2024-03-01 11:38:22 +0300

    course data model

---
+++ Models/Course.cs
@@ -0,0 +1,9 @@
+namespace CourseMgr.Models;
+public class Course
+{
+
+public required string Code {get; set;}
+public required string Title {get; set;}
+public required int Credit { get; set;}
+public string? Description { get; set;}
+}
